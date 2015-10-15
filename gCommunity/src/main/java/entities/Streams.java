dea11546b7cbb06b24e53d10720d5c7a25b60a2f
volco <ayoub.briki@esprit.tn>
2015-10-15 15:51:35 +0100

    added Streams entity

---
+++ gCommunity/src/main/java/entities/Streams.java
@@ -0,0 +1,58 @@
+package entities;
+
+import java.io.Serializable;
+import java.lang.Integer;
+import java.lang.String;
+import java.util.Date;
+
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: Streams
+ *
+ */
+@Entity
+
+public class Streams implements Serializable {
+
+	
+	private Integer id;
+	private Date date_diffusion;
+	private String winner;
+	private Integer viewers;
+	private static final long serialVersionUID = 1L;
+
+	public Streams() {
+		super();
+	}   
+	@Id    
+	public Integer getId() {
+		return this.id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}   
+	public Date getDate_diffusion() {
+		return this.date_diffusion;
+	}
+
+	public void setDate_diffusion(Date date_diffusion) {
+		this.date_diffusion = date_diffusion;
+	}   
+	public String getWinner() {
+		return this.winner;
+	}
+
+	public void setWinner(String winner) {
+		this.winner = winner;
+	}   
+	public Integer getViewers() {
+		return this.viewers;
+	}
+
+	public void setViewers(Integer viewers) {
+		this.viewers = viewers;
+	}
+   
+}
