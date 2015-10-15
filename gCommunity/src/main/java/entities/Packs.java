d8017fb670c16bf4b27ca28807b5de31d2206930
mokhtar0312 <mokhtar.benhmida@esprit.tn>
2015-10-15 15:48:27 +0100

    packs class

---
+++ gCommunity/src/main/java/entities/Packs.java
@@ -0,0 +1,75 @@
+package entities;
+
+import java.io.Serializable;
+import java.lang.Float;
+import java.lang.Integer;
+import java.lang.String;
+import java.util.Date;
+
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: Packs
+ *
+ */
+@Entity
+
+public class Packs implements Serializable {
+
+	
+	private Integer id;
+	private String name;
+	private Float price;
+	private Integer quantity;
+	private String category;
+	private Date datemiseenligne;
+	private static final long serialVersionUID = 1L;
+
+	public Packs() {
+		super();
+	}   
+	@Id    
+	public Integer getId() {
+		return this.id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}   
+	
+	public Date getDatemiseenligne() {
+		return datemiseenligne;
+	}
+	public void setDatemiseenligne(Date datemiseenligne) {
+		this.datemiseenligne = datemiseenligne;
+	}
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}   
+	public Float getPrice() {
+		return this.price;
+	}
+
+	public void setPrice(Float price) {
+		this.price = price;
+	}   
+	public Integer getQuantity() {
+		return this.quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}   
+	public String getCategory() {
+		return this.category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+   
+}
