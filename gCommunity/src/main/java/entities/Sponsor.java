21a19a573ea5dd66394cb6c5793e0bb9b0ee7b7a
AnisElfehem <anis.elfehem@esprit.tn>
2015-10-15 15:12:48 +0100

    sponsor class

---
+++ gCommunity/src/main/java/entities/Sponsor.java
@@ -0,0 +1,56 @@
+package entities;
+
+import java.io.Serializable;
+import java.lang.Integer;
+import java.lang.String;
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: Sponsor
+ *
+ */
+@Entity
+
+public class Sponsor implements Serializable {
+
+	   
+	@Id
+	private Integer id;
+	private String contribution;
+	private String description;
+	private String level;
+	private static final long serialVersionUID = 1L;
+
+	public Sponsor() {
+		super();
+	}   
+	public Integer getId() {
+		return this.id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}   
+	public String getContribution() {
+		return this.contribution;
+	}
+
+	public void setContribution(String contribution) {
+		this.contribution = contribution;
+	}   
+	public String getDescription() {
+		return this.description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}   
+	public String getLevel() {
+		return this.level;
+	}
+
+	public void setLevel(String level) {
+		this.level = level;
+	}
+   
+}
