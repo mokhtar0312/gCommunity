80fbe8be865018c4ca2833131cdbd7843f193da9
mokhtar0312 <mokhtar.benhmida@esprit.tn>
2015-10-16 10:32:26 +0100

    crud SimpleMember

--- gCommunity/src/main/java/services/impl/SimpleMemberServices.java
+++ gCommunity/src/main/java/services/impl/SimpleMemberServices.java
@@ -1,9 +1,16 @@
 package services.impl;
 
+import java.util.List;
+
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
 import services.interfaces.SimpleMemberServicesLocal;
 import services.interfaces.SimpleMemberServicesRemote;
+import entities.SimpleMember;
 
 /**
  * Session Bean implementation class SimpleMemberServices
@@ -11,12 +18,75 @@ import services.interfaces.SimpleMemberServicesRemote;
 @Stateless
 @LocalBean
 public class SimpleMemberServices implements SimpleMemberServicesRemote, SimpleMemberServicesLocal {
-
+	@PersistenceContext
+	private EntityManager entityManager;
     /**
      * Default constructor. 
      */
     public SimpleMemberServices() {
         // TODO Auto-generated constructor stub
     }
+
+	@Override
+	public Boolean addSimpleMember(SimpleMember simpleMember) {
+		Boolean b = false;
+		try {
+			entityManager.persist(simpleMember);
+			b = true;
+		} catch (Exception e) {
+			System.err.println("ouups ...");
+		}
+		return b;
+	}
+
+	@Override
+	public Boolean deleteSimpleMemberById(Integer id) {
+		Boolean b = false;
+		try {
+			entityManager.remove(findSimpleMemberById(id));
+			b = true;
+		} catch (Exception e) {
+			System.err.println("ouups ...");
+		}
+		return b;
+	}
+
+	@Override
+	public SimpleMember findSimpleMemberById(Integer id) {
+		return entityManager.find(SimpleMember.class, id);
+
+	}
+
+	@Override
+	public Boolean updateSimpleMember(SimpleMember simpleMember) {
+		Boolean b = false;
+		try {
+			entityManager.merge(simpleMember);
+			b = true;
+		} catch (Exception e) {
+			System.err.println("ouups ...");
+		}
+		return b;
+	}
+
+	@Override
+	public Boolean deleteEmployee(SimpleMember simpleMember) {
+		Boolean b = false;
+		try {
+			entityManager.remove(entityManager.merge(simpleMember));
+			b = true;
+		} catch (Exception e) {
+			System.err.println("ouups ...");
+		}
+		return b;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<SimpleMember> findAllSimpleMember() {
+		String jpql = "select m from SimpleMember m";
+		Query query = entityManager.createQuery(jpql);
+		return query.getResultList();
+	}
 
 }
--- gCommunity/src/main/java/services/interfaces/SimpleMemberServicesLocal.java
+++ gCommunity/src/main/java/services/interfaces/SimpleMemberServicesLocal.java
@@ -1,8 +1,23 @@
 package services.interfaces;
 
+import java.util.List;
+
 import javax.ejb.Local;
+
+import entities.SimpleMember;
 
 @Local
 public interface SimpleMemberServicesLocal {
+Boolean addSimpleMember(SimpleMember simpleMember);
+	
+	Boolean deleteSimpleMemberById(Integer id);
+
+	SimpleMember findSimpleMemberById(Integer id);
+
+	Boolean updateSimpleMember(SimpleMember simpleMember);
+
+	Boolean deleteEmployee(SimpleMember simpleMember);
+
+	List<SimpleMember> findAllSimpleMember();
 
 }
