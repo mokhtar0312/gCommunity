8c0a93ace2b144dca1e4cbf37877aa4cd8564d94
AnisElfehem <anis.elfehem@esprit.tn>
2015-10-16 10:10:08 +0100

    vote class + refractor project

--- gCommunity/src/main/java/entities/ActiveMember.java
+++ gCommunity/src/main/java/entities/ActiveMember.java
@@ -1,44 +1,44 @@
 package entities;
 
-import entities.SimpleMember;
-
 import java.io.Serializable;
-import java.lang.Integer;
-import java.lang.String;
 import java.util.List;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToOne;
 
 /**
  * Entity implementation class for Entity: ActiveMember
  *
  */
 @Entity
-
 public class ActiveMember extends SimpleMember implements Serializable {
 
-	
 	private Integer phone;
 	private String role;
 	private static final long serialVersionUID = 1L;
-	
+
+	private List<Vote> votes;
 	private List<Event> events;
 	private Membership membership;
-	private List<Equipment>equipments;
-	private List<News>news;
-	private List<Sponsor>sponsors;
-	private List<Streams>streamsss;
+	private List<Equipment> equipments;
+	private List<News> news;
+	private List<Sponsor> sponsors;
+	private List<Streams> streamsss;
+	private Vote vote;
 
 	public ActiveMember() {
 		super();
-	}   
+	}
+
 	public Integer getPhone() {
 		return this.phone;
 	}
 
 	public void setPhone(Integer phone) {
 		this.phone = phone;
-	}   
+	}
+
 	public String getRole() {
 		return this.role;
 	}
@@ -46,47 +46,76 @@ public class ActiveMember extends SimpleMember implements Serializable {
 	public void setRole(String role) {
 		this.role = role;
 	}
+
 	@OneToOne
 	public Membership getMembership() {
 		return membership;
 	}
+
 	public void setMembership(Membership membership) {
 		this.membership = membership;
 	}
+
 	@ManyToMany
 	public List<Event> getEvents() {
 		return events;
 	}
+
 	public void setEvents(List<Event> events) {
 		this.events = events;
 	}
+
 	@ManyToMany
 	public List<Equipment> getEquipments() {
 		return equipments;
 	}
+
 	public void setEquipments(List<Equipment> equipments) {
 		this.equipments = equipments;
 	}
+
 	@ManyToMany
 	public List<News> getNews() {
 		return news;
 	}
+
 	public void setNews(List<News> news) {
 		this.news = news;
 	}
+
 	@ManyToMany
 	public List<Sponsor> getSponsors() {
 		return sponsors;
 	}
+
 	public void setSponsors(List<Sponsor> sponsors) {
 		this.sponsors = sponsors;
 	}
+
 	@ManyToMany
 	public List<Streams> getStreamsss() {
 		return streamsss;
 	}
+
 	public void setStreamsss(List<Streams> streamsss) {
 		this.streamsss = streamsss;
 	}
-   
+
+	public List<Vote> getVotes() {
+		return votes;
+	}
+
+	public void setVotes(List<Vote> votes) {
+		this.votes = votes;
+	}
+
+	@OneToOne
+	public Vote getVote() {
+		return vote;
+	}
+
+	public void setVote(Vote vote) {
+		this.vote = vote;
+	}
+
 }
--- gCommunity/src/main/java/entities/Vote.java
+++ gCommunity/src/main/java/entities/Vote.java
@@ -1,26 +1,70 @@
 package entities;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 
+/**
+ * Entity implementation class for Entity: Vote
+ *
+ */
 @Entity
-public class Vote {
+public class Vote implements Serializable {
 
 	private Integer id;
+	private ActiveMember member;
+	private Boolean voted;
+	private Integer year;
+	private static final long serialVersionUID = 1L;
 
+	private ActiveMember activeMember;
 
 	public Vote() {
 		super();
 	}
 
-	
 	@Id
 	public Integer getId() {
-		return id;
+		return this.id;
 	}
 
 	public void setId(Integer id) {
 		this.id = id;
+	}
+
+	public ActiveMember getMember() {
+		return this.member;
+	}
+
+	public void setMember(ActiveMember member) {
+		this.member = member;
+	}
+
+	public Boolean getVoted() {
+		return this.voted;
+	}
+
+	public void setVoted(Boolean voted) {
+		this.voted = voted;
+	}
+
+	public Integer getYear() {
+		return this.year;
+	}
+
+	public void setYear(Integer year) {
+		this.year = year;
+	}
+
+	@OneToOne(mappedBy = "vote")
+	public ActiveMember getActiveMember() {
+		return activeMember;
+	}
+
+	public void setActiveMember(ActiveMember activeMember) {
+		this.activeMember = activeMember;
 	}
 
 }
