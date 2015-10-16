9d89d4ddaf8bc9c38cee7f5259990ede3da2931f
volco <ayoub.briki@esprit.tn>
2015-10-16 11:52:41 +0100

    Signed-off-by: volco <ayoub.briki@esprit.tn>

---
+++ gCommunity/src/main/java/services/impl/NewsServices.java
@@ -0,0 +1,78 @@
+package services.impl;
+
+import java.util.List;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import services.interfaces.NewsServicesLocal;
+import services.interfaces.NewsServicesRemote;
+import entities.News;
+
+/**
+ * Session Bean implementation class NewsServices
+ */
+@Stateless
+@LocalBean
+public class NewsServices implements NewsServicesRemote, NewsServicesLocal {
+	@PersistenceContext
+	EntityManager entitymanager;
+
+	/**
+	 * Default constructor.
+	 */
+	public NewsServices() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Boolean addNews(News news) {
+		Boolean b = false;
+		try {
+			entitymanager.merge(news);
+			b = true;
+		} catch (Exception e) {
+			System.err.println("couldn't add news");
+		}
+		return b;
+	}
+
+	@Override
+	public News findNewsbyId(Integer Id) {
+		News newsfound = entitymanager.find(News.class, Id);
+		return newsfound;
+	}
+
+	@Override
+	public Boolean updateNews(News news) {
+		Boolean b = false;
+		try {
+			entitymanager.merge(news);
+			b = true;
+		} catch (Exception e) {
+			System.err.println("problem when updating news");
+		}
+		return b;
+	}
+
+	@Override
+	public Boolean deleteNews(News news) {
+		Boolean b = false;
+		try {
+			entitymanager.remove(entitymanager.merge(news));
+			b = true;
+		} catch (Exception e) {
+			System.err.println("ouups ...");
+		}
+		return b;
+	}
+
+	@Override
+	public List<News> DisplayAllNews() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
