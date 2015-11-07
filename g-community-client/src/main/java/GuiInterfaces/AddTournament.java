35f29d2c118e5f4344e2133c5c4b01883dbffa69
Gaz <Gaz@Ghazi>
2015-11-07 15:37:55 +0100

    Double instead of loat in Event Delegate Event Add tournament ( JCalande)
    Signed-off-by: Gaz <Gaz@Ghazi>

--- g-community-client/src/main/java/GuiInterfaces/AddTournament.java
+++ g-community-client/src/main/java/GuiInterfaces/AddTournament.java
@@ -1,16 +1,43 @@
 package GuiInterfaces;
 
+
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
+import javax.swing.JSpinner.DateEditor;
+
+import delegate.EventServiceDelegate;
+import entities.Event;
+
+
+
+
+
+
+
+
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.time.DayOfWeek;
+import java.util.Date;
+
+import com.toedter.calendar.JCalendar;
+import com.toedter.calendar.JDateChooser;
+import com.toedter.calendar.demo.DateChooserPanel;
 
 public class AddTournament {
 
 	private JFrame frame;
-	private JTextField DateText;
+	private JTextField Nametext;
+	private JTextField NumberofText;
+	private JTextField DescritpionText;
+	private JTextField feesText;
+	
 
 	/**
 	 * Launch the application.
@@ -44,7 +71,25 @@ public class AddTournament {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
+		JDateChooser dateChooser = new JDateChooser();
+		dateChooser.setBounds(145, 26, 95, 20);
+		frame.getContentPane().add(dateChooser);
+		
 		JButton btnAddEvent = new JButton("Add Event");
+		btnAddEvent.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Event event= new Event();
+			    
+				event.setDate(dateChooser.getDate());
+				event.setName(Nametext.getText());
+				event.setDescription(DescritpionText.getText());
+				event.setNumberOfParticipants(Integer.parseInt(NumberofText.getText()));
+				event.setFee(Double.parseDouble(feesText.getText()));
+				EventServiceDelegate.DoAddEvent(event);
+				
+				
+			}
+		});
 		btnAddEvent.setBounds(145, 316, 118, 23);
 		frame.getContentPane().add(btnAddEvent);
 		
@@ -68,9 +113,28 @@ public class AddTournament {
 		lblFees.setBounds(23, 235, 46, 14);
 		frame.getContentPane().add(lblFees);
 		
-		DateText = new JTextField();
-		DateText.setBounds(145, 27, 86, 20);
-		frame.getContentPane().add(DateText);
-		DateText.setColumns(10);
+		Nametext = new JTextField();
+		Nametext.setBounds(145, 78, 86, 20);
+		frame.getContentPane().add(Nametext);
+		Nametext.setColumns(10);
+		
+		NumberofText = new JTextField();
+		NumberofText.setBounds(145, 126, 86, 20);
+		frame.getContentPane().add(NumberofText);
+		NumberofText.setColumns(10);
+		
+		DescritpionText = new JTextField();
+		DescritpionText.setBounds(145, 184, 86, 20);
+		frame.getContentPane().add(DescritpionText);
+		DescritpionText.setColumns(10);
+		
+		feesText = new JTextField();
+		feesText.setBounds(145, 232, 86, 20);
+		frame.getContentPane().add(feesText);
+		feesText.setColumns(10);
+		
+		
+	
+		
 	}
 }
